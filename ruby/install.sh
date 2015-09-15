077b7d485763b685cf63a8d6c79ad523148c6b4b
David Pham <davidpham87@gmail.com>
2015-09-15 22:15:51 +0200

    bug: comments for ruby

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,3 +1,5 @@
+# Instal haml and middleman
+
 wget -O ruby-install-0.5.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.5.0.tar.gz
 tar -xzvf ruby-install-0.5.0.tar.gz
 cd ruby-install-0.5.0/

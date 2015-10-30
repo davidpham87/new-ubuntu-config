b0d3628dd2069c09041220806ccc833d485c14ff
David Pham <davidpham87@gmail.com>
2015-10-30 23:24:43 +0100

    feat: Addition of jupyter instead of IPython
    Additionally add all the dependency of IPython

--- python/install.sh
+++ python/install.sh
@@ -4,18 +4,18 @@ sudo apt-get remove python-pip
 sudo easy_install pip
 
 # pandas numpy scipy for python3
-sudo apt-get install python-numpy python-scipy python-matplotlib ipython \
-    ipython-notebook python-pandas python-sympy python-nose \
-    python3-numpy python3-scipy python3-matplotlib \
-    python3-pandas python3-nose ipython3
+sudo apt-get install python-numpy python-scipy python-matplotlib \
+     python-pandas python-sympy python-nose \
+     python3-numpy python3-scipy python3-matplotlib \
+     python3-pandas python3-nose
 
 # Scikit for python 2 and 3
 sudo apt-get install build-essential python-dev python-setuptools \
      libatlas-dev libatlas3gf-base
 
 sudo apt-get install build-essential python3-dev python3-setuptools \
-                     python3-numpy python3-scipy \
-                     libatlas-dev libatlas3gf-base
+     python3-numpy python3-scipy \
+     libatlas-dev libatlas3gf-base
 
 # Set a different blas and lapack
 sudo update-alternatives --set libblas.so.3 \
@@ -24,10 +24,13 @@ sudo update-alternatives --set libblas.so.3 \
 sudo update-alternatives --set liblapack.so.3 \
      /usr/lib/atlas-base/atlas/liblapack.so.3
 
-sudo pip install --user --install-option="--prefix=" -U scikit-learn
+sudo pip install --install-option="--prefix=" -U scikit-learn
 
-# Sphinx for documentation
-sudo apt-get install python-sphinx
+# Install jinja2, sphinx (documentation), pyzmq, pygments, tornado, nose
+sudo pip install -U ipython[all]
+
+# jupyiter for interactive notebooks
+sudo pip install -U jupyter
 
 # jedi for emacs autocompletion
 sudo pip install -U jedi

9bf20c170a11c07a3748c09878ec35c8ee63171d
David Pham <davidpham87@gmail.com>
2015-12-06 00:09:11 +0100

    Simplification of emacs installation

--- emacs/install.sh
+++ emacs/install.sh
@@ -15,13 +15,17 @@ cd emacs-$EMACS_VERSION && ./configure && make && sudo make install
 echo 'export EDITOR="emacs -Q"' >> ~/.bashrc
 echo 'export TERM=xterm-256color' >> ~/.bashrc
 
+# Clean
+rm -r emacs-$EMACS_VERSION
+rm emacs-$EMACS_VERSION.tar.*
+
 # Install emacs speaks statistics
 sudo apt-get $APT_INSTALL_ARGS install ess
 
 # Download and use my default emacs config
-git clone https://github.com/davidpham87/emacs-data-analysis-config.git ~/
 rm -r ~/.emacs.d
-mv -f ~/emacs-data-analysis-config ~/.emacs.d
+git clone https://github.com/davidpham87/emacs-data-analysis-config.git ~/.emacs.d
 mv ~/.emacs.d/.emacs-live.el ~/
 
-git clone https://github.com/shime/emacs-livedown.git ~/.emacs.d/packs/lang-code/lib/
+# Livedown
+git clone https://github.com/shime/emacs-livedown.git ~/.emacs.d/packs/lang-code/lib/emacs-livedown

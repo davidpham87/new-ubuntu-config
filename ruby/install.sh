80aec0b9f07e11985d47349708edf512f2923eb4
David Pham <davidpham87@gmail.com>
2015-12-06 00:11:53 +0100

    Update of Ruby installation

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,10 +1,15 @@
 # Instal haml and middleman
+VERSION="0.5.0"
 
-wget -O ruby-install-0.5.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.5.0.tar.gz
-tar -xzvf ruby-install-0.5.0.tar.gz
-cd ruby-install-0.5.0/
+wget -O ruby-install-$VERSION.tar.gz https://github.com/postmodern/ruby-install/archive/v$VERSION.tar.gz
+tar -xzvf ruby-install-$VERSION.tar.gz
+cd ruby-install-$VERSION/
 sudo make install
 
 sudo ruby-install ruby
-sudo gem install 'html2haml'
+sudo gem install html2haml
 sudo gem install middleman
+
+# clean
+rm -r ruby-install-$VERSION(
+rm ruby-install-$VERSION.tar.gz

8e52c5c9d329e7009680c01284b3adfccc5d89e6
David Pham <davidpham87@gmail.com>
2015-12-31 11:27:27 +0100

    fix: git recursive clone of emacs.d
    Also addition of a better bash prompt (PS1)

--- emacs/install.sh
+++ emacs/install.sh
@@ -14,6 +14,7 @@ cd emacs-$EMACS_VERSION && ./configure && make && sudo make install
 
 echo 'export EDITOR="emacs -Q"' >> ~/.bashrc
 echo 'export TERM=xterm-256color' >> ~/.bashrc
+echo 'export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\e[1;34m\]\u@\h\[\e[m\]: \[\e[1;31m\]\w\[\e[m\]\n\$ "' >> ~/.bashrc
 
 # Clean
 rm -r emacs-$EMACS_VERSION
@@ -24,8 +25,5 @@ sudo apt-get $APT_INSTALL_ARGS install ess
 
 # Download and use my default emacs config
 rm -r ~/.emacs.d
-git clone https://github.com/davidpham87/emacs-data-analysis-config.git ~/.emacs.d
+git clone --recursive https://github.com/davidpham87/emacs-data-analysis-config.git ~/.emacs.d
 mv ~/.emacs.d/.emacs-live.el ~/
-
-# Livedown
-git clone https://github.com/shime/emacs-livedown.git ~/.emacs.d/packs/lang-code/lib/emacs-livedown

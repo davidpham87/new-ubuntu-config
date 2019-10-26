6f8921891e273f519a05cb3b8dfd8dbe7a2b87d9
David Pham <davidpham87@gmail.com>
2019-10-26 17:18:32 +0200

    feat: update

--- java/install.sh
+++ java/install.sh
@@ -1,4 +1,2 @@
 echo "Installing Java"
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get -y --force-yes install oracle-java8-installer
+sudo apt-get install openjdk-8-jre

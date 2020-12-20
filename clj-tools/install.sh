c5bbe8d54a62f0dbee93e369599be3c066a46c73
David Pham <davidpham87@gmail.com>
2020-12-20 11:34:06 +0100

    feat: update

--- clj-tools/install.sh
+++ clj-tools/install.sh
@@ -8,3 +8,11 @@ echo "Installing Clj-kondo"
 curl -s https://raw.githubusercontent.com/borkdude/clj-kondo/master/script/install-clj-kondo > clj-kondo-install
 bash clj-kondo-install
 rm clj-kondo-install
+
+
+echo "Installing Jet"
+curl -s https://raw.githubusercontent.com/borkdude/jet/master/install> jet-install
+bash jet-install
+rm jet-install
+
+echo "Install grasp"
--- emacs/install.sh
+++ emacs/install.sh
@@ -1,14 +1,29 @@
 echo "Installing Emacs"
 
 # Essential building stuff
-sudo apt -y install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev
+sudo apt -y install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev \
+     libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev
 
 # for pdf, see https://github.com/politza/pdf-tools
 sudo apt -y install zlib1g-dev libpoppler-glib-dev libpoppler-private-dev imagemagick
 sudo apt -y install libgnutls28-dev libwebkit2gtk-4.0-dev libwebkit2gtk-4.0
 
-sudo apt -y install snapd
-sudo snap install emacs --classic
+# https://emacs.stackexchange.com/questions/59538/compile-emacs-from-feature-native-comp-gccemacs-branch-on-ubuntu
+sudo apt install libxpm-dev libgif-dev libjpeg-dev libpng-dev libtiff-dev \
+     libx11-dev libncurses5-dev automake autoconf texinfo libgtk2.0-dev
+sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
+sudo apt install gcc-10 g++-10 libgccjit0 libgccjit-10-dev libjansson4 \
+     libjansson-dev
+git clone git://git.sv.gnu.org/emacs.git
+git checkout feature/native-comp
+export CC=/usr/bin/gcc-10 CXX=/usr/bin/gcc-10
+./autogen.sh
+./configure --with-nativecomp --with-json CFLAGS="-O3 -mtune=native -march=native -fomit-frame-pointer"
+make -j16 NATIVE_FULL_AOT=1
+make install
+
+# sudo apt -y install snapd
+# sudo snap install emacs --classic
 
 echo 'export EDITOR="emacs -Q"' >> ~/.bashrc
 echo 'export TERM=xterm-256color' >> ~/.bashrc

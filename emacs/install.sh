146dda32ac563584a4013bfcc85d791dc33ae325
David Pham <davidpham87@gmail.com>
2022-01-30 18:59:35 +0100

    feat: update emacs

--- emacs/install.sh
+++ emacs/install.sh
@@ -1,24 +1,34 @@
 echo "Installing Emacs"
 
-# Essential building stuff
-sudo apt -y install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev \
-     libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev
-
-# for pdf, see https://github.com/politza/pdf-tools
-sudo apt -y install zlib1g-dev libpoppler-glib-dev libpoppler-private-dev imagemagick
-sudo apt -y install libgnutls28-dev libwebkit2gtk-4.0-dev libwebkit2gtk-4.0
-
 # https://emacs.stackexchange.com/questions/59538/compile-emacs-from-feature-native-comp-gccemacs-branch-on-ubuntu
-sudo apt install libxpm-dev libgif-dev libjpeg-dev libpng-dev libtiff-dev \
-     libx11-dev libncurses5-dev automake autoconf texinfo libgtk2.0-dev
+
 sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
-sudo apt install gcc-10 g++-10 libgccjit0 libgccjit-10-dev libjansson4 \
+sudo apt update
+
+# Essential building stuff
+# for pdf, see https://github.com/politza/pdf-tools
+sudo apt -y install \
+     build-essential texinfo libx11-dev \
+     libxpm-dev libjpeg-dev \
+     libpng-dev libgif-dev libtiff-dev libgtk-3-dev libncurses-dev \
+     zlib1g-dev libpoppler-glib-dev libpoppler-private-dev imagemagick \
+     libgnutls28-dev libwebkit2gtk-4.0-dev libwebkit2gtk-4.0-37 \
+     libxpm-dev libgif-dev libjpeg-dev libpng-dev libtiff-dev \
+     libx11-dev libncurses5-dev automake autoconf texinfo libgtk2.0-dev \
+     gcc-10 g++-10 libgccjit0 libgccjit-10-dev libjansson4 \
      libjansson-dev
+
 git clone git://git.sv.gnu.org/emacs.git emacs-source
 cd emacs-source
 export CC=/usr/bin/gcc-10 CXX=/usr/bin/gcc-10
 ./autogen.sh
-./configure --with-json --with-tiff=ifavailable --with-svg --with-native-compilation  CFLAGS="-O3 -mtune=native -march=native -fomit-frame-pointer"
+./configure --with-json \
+            --with-tiff=ifavailable \
+            --with-svg \
+            --with-pgtk \
+            --with-native-compilation \
+            CFLAGS="-O3 -mtune=native -march=native -fomit-frame-pointer"
+
 make -j16 NATIVE_FULL_AOT=1
 sudo make install
 

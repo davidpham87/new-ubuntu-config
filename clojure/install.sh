32024b1a4f2d66f154010a5459e793457ad42f03
david <davidpham87@gmail.com>
2022-02-22 13:11:16 +0100

    feat: update clojure

--- clojure/install.sh
+++ clojure/install.sh
@@ -1,18 +1,10 @@
 echo "Installing Clojure and Lein"
 sudo apt install curl
 
-curl -O https://download.clojure.org/install/linux-install-1.10.3.933.sh
-chmod +x linux-install-1.10.3.933.sh
-sudo ./linux-install-1.10.3.933.sh
-rm linux-install-1.10.3.933.sh
+VERSION=1.10.3.1075
+SCRIPT=linux-install-$VERSION.sh
 
-cd ~/
-mkdir -p ~/bin
-wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
-mv lein ~/bin/
-chmod a+x ~/bin/lein
-
-# echo "Installing drip"
-# curl -L https://raw.githubusercontent.com/ninjudd/drip/master/bin/drip > ~/bin/drip
-# chmod 755 ~/bin/drip
-# sudo echo "LEIN_JAVA_CMD=\${LEIN_JAVA_CMD-drip}" >> ~/.lein/leinrc
+curl -O https://download.clojure.org/install/$SCRIPT
+chmod +x $SCRIPT
+sudo ./$SCRIPT
+rm $SCRIPT
